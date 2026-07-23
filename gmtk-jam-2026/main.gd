c55dd23df1f695aca80320143360bbd837f46586
Baba T <118652556+BabaT130@users.noreply.github.com>
2026-07-23 13:57:24 -0700

    Delete gmtk-jam-2026 directory

--- gmtk-jam-2026/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _input(event: InputEvent) -> void:
-	get_node("ProgressBar").value -= 2
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_node("RigidBody2D").set_deferred("freeze", false)
