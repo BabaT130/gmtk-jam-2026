3a16eb3ac044748093c9bc05b1173a2f9bff8869
Baba T <118652556+BabaT130@users.noreply.github.com>
2026-08-20 16:28:30 -0700

    Delete gmtk-jam-2026 directory

--- gmtk-jam-2026/box_spawner.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var box = preload("res://box.tscn")
-
-func _process(delta: float) -> void:
-	if get_parent().get_node("ProgressBar").value <= 0:
-		var spawned_box = box.instantiate()
-		spawned_box.position = $Marker2D.position
-		add_child(spawned_box)
